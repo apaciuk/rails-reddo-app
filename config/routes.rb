6c981f8cbc65f04bbed8b0499a197c284fc1edd5
Paul Anthony McGowan <xhostcom@gmail.com>
2021-09-13 08:04:05 +0100

    Nested Post

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  resources :posts
-  resources :communities
   get '/privacy', to: 'home#privacy'
   get '/terms', to: 'home#terms'
 authenticate :user, lambda { |u| u.admin? } do
@@ -15,6 +13,10 @@ end
   resources :notifications, only: [:index]
   resources :announcements, only: [:index]
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  resources :communities do
+    resources :posts
+  end
+
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
